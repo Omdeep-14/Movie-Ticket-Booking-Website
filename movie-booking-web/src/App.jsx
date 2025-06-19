6d3a94789a666443acd0f2353c3d7c105c046352
Omdeep-14 <202654484+Omdeep-14@users.noreply.github.com.>
2025-06-19 23:34:20 +0530

    frontend half done

--- movie-booking-web/src/App.jsx
+++ movie-booking-web/src/App.jsx
@@ -2,7 +2,36 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import { Route, Routes, useLocation } from "react-router";
+import Home from "./Pages/Home";
+import Movies from "./Pages/Movies";
+import MovieDetails from "./Pages/MovieDetails";
+import SeatLayout from "./Pages/SeatLayout";
+import MyBookings from "./Pages/MyBookings";
+import Favourite from "./Pages/Favourite";
+import { Toaster } from "react-hot-toast";
+import Footer from "./Components/Footer";
 
-function App() {}
+function App() {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favourite" element={<Favourite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+}
 
 export default App;
---
+++ movie-booking-web/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../Components/HeroSection";
+import FeaturedSection from "../Components/FeaturedSection";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <FeaturedSection />
+    </>
+  );
+}
+
+export default Home;
